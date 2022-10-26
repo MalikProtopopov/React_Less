7d7a789a6b036e4812190026a009459524c7c510
Малик Протопопов <malikprotopopov@MacBook-Pro-Malik.local>
2022-10-26 18:08:53 +0300

    The first sample of project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img className='imageHeader' src='https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg?uselang=ru'></img>
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return  <div className='content'>
+        <div>
+            <img className='contentImage' src='https://png.pngtree.com/background/20220715/original/pngtree-c4d-cyberpunk-city-background-picture-image_1623401.jpg'></img>
+        </div>
+        <div>
+            <img className='avatar' src='https://i.pinimg.com/originals/30/96/85/309685f2f0200b7431c20f1cb711360e.jpg'></img>
+            description
+        </div>
+        <div>
+            My posts
+            <div>
+                New posts
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+
+    </div>
+}
+
+export default Profile
